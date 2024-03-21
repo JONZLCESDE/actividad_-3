8ca1eadc9922bc6e38e8379710caaad16cd861f8
JoncesdeZL <163427101+JONZLCESDE@users.noreply.github.com>
2024-03-20 22:09:37 -0500

    Add files via upload

---
+++ actividad 3.java
@@ -0,0 +1,48 @@
+
+
+public class main {
+
+    public static void main(String[] args) {
+        
+   String[] dias = {"lunes", "martes", "miercoles", "jueves","viernes", "sabado", "domingo"};
+
+
+   int[] diaMes =  {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
+   
+   
+   int[] año = {2020,2021,2022,2023,2024,2025,2026,2027,2028,2029,2030};
+
+
+    String[] meses = new String[12];
+    meses[0] = "enero";
+    meses[0] = "febrero";
+    meses[0] = "marzo";
+    meses[0] = "abril";
+    meses[0] = "mayo";
+    meses[0] = "junio";
+    meses[0] = "julio";
+    meses[0] = "agosto";
+    meses[0] = "septiembre";
+    meses[0] = "octubre";
+    meses[0] = "noviembre";
+    meses[0] = "diciembre";
+
+
+    String[] meses = {"enero", "febrero" , "marzo", "abril" , "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
+
+
+
+
+
+    
+
+
+
+
+
+
+
+    }
+
+    
+}
